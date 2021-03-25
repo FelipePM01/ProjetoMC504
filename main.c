dac41daeea9cc1189b3144ff35167a5b234ad345
Alec <alecsantos96@gmail.com>
2021-03-25 15:14:36 -0300

    Atualiza main()

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "inputoutput.h"
+
+#define N_THR = 10
+
+void* f_thread(void *v) {
+  int id = (int) v;
+  printf("Thread %d\n", id);  
+  return NULL; 
+} 
 
 int main(){
+    int i, j;
+    pthread_t thr[N_THR][N_THR];
+    for (i = 0; i < N_THR; i++){
+        for (j = 0; j < N_THR;j++){
+            pthread_create(&thr[i][j], NULL, f_thread, (void*) i);
+        }
+    }
+    pthread_t thr[N_THR];
     printf("Hello World!");
 }
