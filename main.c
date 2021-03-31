40bc4b6ca5fcd3a5a352ab3aa25b7f29173ad511
Felipe <fpachecomanoel@uol.com.br>
2021-03-31 15:46:58 -0300

    correcao da entrada

--- main.c
+++ main.c
@@ -14,19 +14,20 @@ typedef struct {
 void entrada(int* saida,int n){
     
     
-    for(int i=0;i<n;i++){
+    for(int i=1;i<n-1;i++){
         
-        if((i<(n-1))&&i>0){
+       
 
            
-            for(int j=1;j<n-2;j++){
-                scanf("%d ",&saida[i*n+j]);
-            }
-            scanf("%d\n",&saida[i*n+n-2]);
+        for(int j=1;j<n-1;j++){
+            scanf("%d",&saida[i*n+j]);
         }
+           
+        
     
        
     }
+    
     for(int i=0;i<25;i++)
         printf("%d ",saida[i]);
     printf("\n");
