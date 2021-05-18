2f961ff7d646a294ec6806f1f87964575205cebd
Alec <alecsantos96@gmail.com>
2021-05-18 12:53:24 -0300

    Melhorias f_cozinheiro()

--- projeto2/main.c
+++ projeto2/main.c
@@ -56,11 +56,20 @@ typedef struct {
 } Args;
 
 void cozinhar(){
+    printf("Cozinhando...");
     usleep(2000);
 }
+
 void cortar(){
+    printf("Cortando...");
     usleep(2000);
 }
+
+void preparar_peixe_frito(){
+    printf("Preparando Peixe Frito...");
+    usleep(2000);
+}
+
 void entregar(Armazem* armazem,int resultado,sem_t* checagem){//
     sem_wait(armazem->acesso);
     (armazem->pronto)[resultado]++;
@@ -114,31 +123,34 @@ void f_cozinheiro(void * args){ //working=variavel q guarda se acabou o programa
         sem_post(tarefa->acesso);
 
         if(tarefa_atual==0){//cozinhar arroz
-
             pegar(armazem,tarefa_atual);
             sem_wait(panelas);
-            cortar(tarefa);
+            cozinhar();
             sem_pos(panelas);
             entregar(armazem,tarefa_atual,checagem);
-
         }
-            pegar(armazem,tarefa_atual);
         else if(tarefa_atual==1){//preparar camarao
-            sem_wait(facas);
-            cortar(tarefa_atual);
-            sem_pos(facas);
-            sem_wait(panelas);
-            cozinhar(tarefa);
-            sem_pos(panelas);
-            entregar(armazem,tarefa_atual,checagem);
-
-
-        }
-        else if(tarefa==2){//cortar peixe
             pegar(armazem,tarefa_atual);
             sem_wait(facas);
-            cortar(tarefa_atual);
+            cortar();
             sem_pos(facas);
+            sem_wait(panelas);
+            cozinhar();
+            sem_pos(panelas);
+            entregar(armazem,tarefa_atual,checagem);
+        }
+        else if(tarefa_atual==2){//cortar peixe
+            pegar(armazem,tarefa_atual);
+            sem_wait(facas);
+            cortar();
+            sem_pos(facas);
+            entregar(armazem,tarefa_atual,checagem);
+        }
+        else if(tarefa_atual==3){//preparar peixe frito
+            pegar(armazem,tarefa_atual);
+            sem_wait(panelas);
+            preparar_peixe_frito();
+            sem_pos(panelas);
             entregar(armazem,tarefa_atual,checagem);
         }
         sem_wait(tarefas->possui_tarefas);
